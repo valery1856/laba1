e894a350e52bc5bd51d3d9e2398429c117058ed0
valery1856 <valery.utin@yandex.ru>
2019-05-28 13:40:00 +0300

    new commit

---
+++ suzen19.sh
@@ -0,0 +1,16 @@
+
+#!bin/bash/
+mkdir Documents
+cd Documents/
+mkdir projects
+cd projects/
+mkdir lab19
+cd lab19/
+mkdir using
+cd using/
+mkdir simple
+cd simple/
+mkdir bash
+cd bash/
+mkdir commands
+cd commands/
