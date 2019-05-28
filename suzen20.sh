a2fadbd21d1928108f38ccfe9ca4412e6606c249
valery1856 <valery.utin@yandex.ru>
2019-05-28 13:42:20 +0300

    new commit

---
+++ suzen20.sh
@@ -0,0 +1,41 @@
+#!bin/bash/
+/home/suzen # cd ..
+/home # ls
+/home # cd john/
+/home/john # ls
+/home/john # cd Desktop/
+/home/john/Desktop # ls
+/home/john/Desktop # ls
+/home/john/Desktop # cd films/
+/home/john/Desktop/films # ls
+/home/john/Desktop/films # cd ololo/
+/home/john/Desktop/films/ololo # ls
+/home/john/Desktop/films/ololo # cd ..
+/home/john/Desktop/films # cd ..
+/home/john/Desktop # ls
+/home/john/Desktop # cd films/
+/home/john/Desktop/films # rmdir ololo/
+/home/john/Desktop/films # cd ..
+/home/john/Desktop # rmdir films/
+/home/john/Desktop # cd music/
+/home/john/Desktop/music # rmdir ololo/
+/home/john/Desktop/music # cd ..
+/home/john/Desktop # rmdir music/
+/home/john/Desktop # cd ..
+/home/john # rmdir Desktop/
+/home/john # rm file1
+/home/john # rm file2
+/home/john # rm file3 
+/home/john # cd Documents/
+/home/john/Documents # cd music/
+/home/john/Documents/music # rmdir azaza/
+/home/john/Documents/music # cd ..
+/home/john/Documents # rmdir music/
+/home/john/Documents # cd films/
+/home/john/Documents/films # rmdir azaza/
+/home/john/Documents/films # cd ..
+/home/john/Documents # rmdir films/
+/home/john/Documents # cd ..
+/home/john # rmdir Documents/
+/home/john # cd ..
+/home # cd suzen/
