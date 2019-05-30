4b57a88a418c9bd09d8bd505279a70d456dc0b07
valery1856 <valery.utin@yandex.ru>
2019-05-30 09:56:55 +0300

    new commit

---
+++ suzen23.sh
@@ -0,0 +1,8 @@
+#!bin/bash/
+/home/suzen/source # mv *.log /home/suzen/destination/
+/home/suzen/destination # mv nginx1.back.log nginx1.log.back
+/home/suzen/destination # mv nginx2.back.log nginx2.log.back
+/home/suzen/destination # mv nginx3.back.log nginx3.log.back
+/home/suzen/destination # mv nginx4.back.log nginx4.log.back
+/home/suzen/destination # mv nginx5.back.log nginx5.log.back
+
