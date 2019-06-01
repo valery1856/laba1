fffcd43008a7f30b2a15b67f2aee4ec25468b23c
valery1856 <50926977+valery1856@users.noreply.github.com>
2019-06-01 13:54:02 +0300

    Update suzen19.sh

--- suzen19.sh
+++ suzen19.sh
@@ -1,16 +1,5 @@
 
 #!bin/bash/
-mkdir Documents
-cd Documents/
-mkdir projects
-cd projects/
-mkdir lab19
-cd lab19/
-mkdir using
-cd using/
-mkdir simple
-cd simple/
-mkdir bash
-cd bash/
-mkdir commands
-cd commands/
+mkdir -p Documents/projects/lab19/using/simple/bash/commands
+var=$(ls Documents/projects/lab19/using/simple/bash/commands)
+echo "flag is: $var"
